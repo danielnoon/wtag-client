0d67ce63b26bcf2d0f10ed1abf58fa96ff7923be
Daniel Noon <supercookies@outlook.com>
2019-08-02 23:22:16 -0500

    final login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { sha256 } from 'sha.js';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  username: string;
+  password: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  async login() {
+    console.log(`Hello, ${this.username}!`);
+    const hashedPw = new sha256().update(this.password + 'wtag').digest('hex');
+    console.log(`Your hashed password is ${hashedPw}`);
+    const result = await fetch(
+      'https://wtag-api.supermegadex.net/api/v1/login',
+      {
+        method: 'post',
+        body: JSON.stringify({ username: this.username, password: hashedPw }),
+        headers: { 'Content-Type': 'application/json' }
+      }
+    );
+    console.log(await result.json());
   }
-
 }
