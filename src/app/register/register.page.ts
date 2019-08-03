a764dce5a55000b6a5a3028b41a0455df57ddb96
Daniel Noon <supercookies@outlook.com>
2019-08-02 23:32:14 -0500

    consistency is key

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { sha256 } from 'sha.js';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  styleUrls: ['./register.page.scss']
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  username = '';
+  password = '';
+  accessCode = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  async register() {
+    const hashedPassword = new sha256()
+      .update(this.password + 'wtag')
+      .digest('hex');
+    const response = await fetch(
+      'https://wtag-api.supermegadex.net/api/v1/register',
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: this.username,
+          password: hashedPassword,
+          accessCode: this.accessCode
+        })
+      }
+    );
+    console.log(await response.json());
   }
-
 }
